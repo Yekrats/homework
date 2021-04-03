704bf843e9a1f2dc3031da88719eb3be12007be1
Scott Starkey <yekrats@gmail.com>
2021-04-03 08:50:47 -0400

    parse-line function

--- src/homework/core.clj
+++ src/homework/core.clj
@@ -1,7 +1,15 @@
 (ns homework.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.string :as str]))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn parse-line
+  "Basic parsing of a line (string) into initial map."
+  [string]
+  (->> (str/split string #"[\|\s\,]+") ;; parse basic string splitting by the pipe, space, or comma characters
+       (zipmap [:last-name :first-name :email :favorite-color :date-of-birth])))
--- test/homework/core_test.clj
+++ test/homework/core_test.clj
@@ -2,6 +2,16 @@
   (:require [clojure.test :refer :all]
             [homework.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest basic-parse
+  (testing
+    (is (= (parse-line "Starkey | Scott | name@gmail.com | green | 01/01/2000") ; pipe-delimited data
+           {:last-name "Starkey", :first-name "Scott", :email "name@gmail.com", :favorite-color "green", :date-of-birth "01/01/2000"}))
+
+    (is (= (parse-line "Starkey, Scott, name@gmail.com, green, 01/01/2000") ; comma-delimited data
+           {:last-name "Starkey", :first-name "Scott", :email "name@gmail.com", :favorite-color "green", :date-of-birth "01/01/2000"}))
+
+    (is (= (parse-line "Starkey Scott name@gmail.com green 01/01/2000") ; space-delimited data
+           {:last-name "Starkey", :first-name "Scott", :email "name@gmail.com", :favorite-color "green", :date-of-birth "01/01/2000"}))
+
+
+    ))
