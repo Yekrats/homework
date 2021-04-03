54a0f9240751dfbaa1d6587ccced43099501d405
Scott Starkey <yekrats@gmail.com>
2021-04-03 10:19:07 -0400

    parse-file function; move test files to /resources

--- src/homework/core.clj
+++ src/homework/core.clj
@@ -13,3 +13,13 @@
   [string]
   (->> (str/split string #"[\|\s\,]+") ;; parse basic string splitting by the pipe, space, or comma characters
        (zipmap [:last-name :first-name :email :favorite-color :date-of-birth])))
+
+(defn parse-file
+  "Parsing each of the lines of a file. 'file-name' is considered a string.
+  Returns a map of each line processed through parse-line."
+  ; Example:  (parse-file "./resources/comma-data.csv")
+  [file-name]
+  {:pre [(= (type file-name) java.lang.String)]}
+  (->> (slurp file-name)
+       (str/split-lines)
+       (map parse-line)))
--- test/homework/core_test.clj
+++ test/homework/core_test.clj
@@ -12,6 +12,28 @@
 
     (is (= (parse-line "Starkey Scott name@gmail.com green 01/01/2000") ; space-delimited data
            {:last-name "Starkey", :first-name "Scott", :email "name@gmail.com", :favorite-color "green", :date-of-birth "01/01/2000"}))
-
-
     ))
+
+(deftest file-parse
+  (testing
+      (is [= '({:last-name "Starkey",
+  :first-name "Scotticus",
+  :email "yekrats@gmail.com",
+  :favorite-color "green",
+  :date-of-birth "01/01/2000"}
+ {:last-name "McClane",
+  :first-name "John",
+  :email "jmcclane@nypd.com",
+  :favorite-color "brown",
+  :date-of-birth "12/24/1960"}
+ {:last-name "Gennero",
+  :first-name "Holly",
+  :email "gennero@nakatomi.com",
+  :favorite-color "yellow",
+  :date-of-birth "6/20/1960"}
+ {:last-name "Gruber",
+  :first-name "Hans",
+  :email "volksfrei.de",
+  :favorite-color "blue",
+  :date-of-birth "2/14/1958"})
+         (parse-file "./resources/comma-data.csv")])))
