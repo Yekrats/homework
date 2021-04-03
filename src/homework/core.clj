7c5921458a770b2ec7aae49c4adf8606f4c79f70
Scott Starkey <yekrats@gmail.com>
2021-04-03 16:05:52 -0400

    add parse-date function; sort-by-email-desc stub

--- src/homework/core.clj
+++ src/homework/core.clj
@@ -1,25 +1,40 @@
 (ns homework.core
   (:gen-class)
-  (:require [clojure.string :as str]))
-
+  (:require [clojure.string :as str])
+  (:import [java.text SimpleDateFormat]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
 
+(defn parse-date
+  "Formats date to Java date object"
+  [string]
+    (.parse
+      (SimpleDateFormat. "MM/dd/yyyy")
+      string))
+
 (defn parse-line
   "Basic parsing of a line (string) into initial map."
   [string]
-  (->> (str/split string #"[\|\s\,]+") ;; parse basic string splitting by the pipe, space, or comma characters
-       (zipmap [:last-name :first-name :email :favorite-color :date-of-birth])))
+  (as-> (str/split string #"[\|\s\,]+") $ ;; parse basic string splitting by the pipe, space, or comma characters
+      (zipmap [:last-name :first-name :email :favorite-color :date-of-birth] $) ; combine fields with data
+      (update-in $ [:date-of-birth] parse-date))
+       )
 
 (defn parse-file
   "Parsing each of the lines of a file. 'file-name' is considered a string.
   Returns a map of each line processed through parse-line."
   ; Example:  (parse-file "./resources/comma-data.csv")
   [file-name]
-  {:pre [(= (type file-name) java.lang.String)]}
+  {:pre [(= true (string? file-name))]}
   (->> (slurp file-name)
        (str/split-lines)
        (map parse-line)))
+
+(defn sort-by-email-desc
+  "Sorts a record map by email order descending (then by last name ascending).
+   Returns new map in sorted order."
+  [record-map]
+  )
